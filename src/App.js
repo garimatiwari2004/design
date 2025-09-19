94ab470a1a45b33fbdd5bfeada17c1a102faaf08
Debostuti Das <debostutidas@Debostutis-MacBook-Pro.local>
2025-09-19 08:58:48 +0530

    Updated Payments page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
@@ -7,33 +6,24 @@ import Login from './components/login';
 import CustomerLogin from './components/customerlogin';
 import ServiceLogin from './components/serviceproviderlogin';
 import Success from './components/success';
-import Pricing from './components/allservices';
 import AllServices from './components/allservices';
-
+import Payments from './components/payments';   // ✅ Payments page
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-     
-
-      <Route path="/" element={<Hero />} />
-      <Route path="/home" element={<Hero />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/customerlogin" element={<CustomerLogin/>} />
-      <Route path="/serviceproviderlogin" element={<ServiceLogin/>} />
-      <Route path="/success" element={<Success/>}/>
-      <Route path="/allservices" element={<AllServices/>}/>
-      
-      {/* <Route path="/about" element={<About />} /> */}
-      
-    
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/home" element={<Hero />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/customerlogin" element={<CustomerLogin />} />
+        <Route path="/serviceproviderlogin" element={<ServiceLogin />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/allservices" element={<AllServices />} />
+        <Route path="/payments" element={<Payments />} />  {/* ✅ New route */}
+      </Routes>
     </>
-      
-
-    
   );
 }
 
